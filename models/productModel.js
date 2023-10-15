78dfe69ab71e7a7f90e46db4e1744f914a316629
Anand Kumar <121444538+AnandKumar1008@users.noreply.github.com>
2023-10-15 18:00:20 +0530

    Update productModel.js

--- models/productModel.js
+++ models/productModel.js
@@ -1,28 +1,28 @@
-// const mongoose = require('mongoose');
-
-// const productSchema = new mongoose.Schema({
-//   //name will be a string with required true
-//   //price will be a Number with required true
-//   // quantity will be a number with required true
-// });
-
-// module.exports = mongoose.model('Product', productSchema);
-
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  price: {
-    type: Number,
-    required: true
-  },
-  quantity: {
-    type: Number,
-    required: true
-  }
+  //name will be a string with required true
+  //price will be a Number with required true
+  // quantity will be a number with required true
 });
 
 module.exports = mongoose.model('Product', productSchema);
+
+// const mongoose = require('mongoose');
+
+// const productSchema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     required: true
+//   },
+//   price: {
+//     type: Number,
+//     required: true
+//   },
+//   quantity: {
+//     type: Number,
+//     required: true
+//   }
+// });
+
+// module.exports = mongoose.model('Product', productSchema);
